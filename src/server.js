4af1c4d2e3a87e1374a9b6e4b78eaa06051f93bd
ihanseul731 <61959771+YiHanSeul@users.noreply.github.com>
2022-01-05 11:23:26 +0900

    middleware

--- src/server.js
+++ src/server.js
@@ -2,16 +2,18 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
-/** application (get,request)설정 */
+/**Middle ware */
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
 
+/** application (get,request)설정 */
 const handleHome = (req, res) => {
-  return res.send("<h1>I still study</h1>");
+  return res.send("<h1>This is middlewares</h1>");
 };
-const handleLogin = (req, res) => {
-  return res.send({ message: "handle login page" });
-};
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+
+app.get("/", logger, handleHome);
 
 /** 외부 접속 Listen*/
 const handleListening = () =>
