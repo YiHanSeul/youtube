9a6dc86923217380baf8de386c64463af9b946c4
ihanseul731 <61959771+YiHanSeul@users.noreply.github.com>
2022-01-05 15:33:40 +0900

    morgan

--- src/server.js
+++ src/server.js
@@ -1,20 +1,30 @@
 import express from "express";
+import morgan from "morgan";
 const PORT = 4000;
 const app = express();
+const logger = morgan("dev");
 
 /**Middle ware */
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
+const routerLogger = (req, res, next) => {
+  console.log(`PATH ${req.path}`);
   next();
 };
-
+const methodLogger = (req, res, next) => {
+  console.log(`METHOD ${req.method}`);
+  next();
+};
 /** application (get,request)설정 */
-const handleHome = (req, res) => {
-  return res.send("<h1>This is middlewares</h1>");
+const home = (req, res) => {
+  console.log("I will respond.");
+  return res.send("hello");
 };
 
-app.get("/", logger, handleHome);
-
+const login = (req, res) => {
+  return res.send("Login");
+};
+app.use(logger);
+app.get("/", home);
+app.get("/login", login);
 /** 외부 접속 Listen*/
 const handleListening = () =>
   console.log(`Server listenting on port http://localhost:${PORT}👂`);
