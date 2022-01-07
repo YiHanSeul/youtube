5c073111c4ad15fd571c591b0e198d8e6fdac6b4
ihanseul731 <61959771+YiHanSeul@users.noreply.github.com>
2022-01-07 14:07:44 +0900

    controller planing

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 #youtube 2022 update
+
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+/users/edit -> Edit user
+/user/delete -> Delete user
+
+/videos/:id -> Watch Video
+/videos/edit -> Edit Video
+/videos/delete -> Delete Video
+/viedos/comments ->Comment on a Video
+/videos/comments/delete -> Delete AComment of a Video/
--- src/server.js
+++ src/server.js
@@ -1,30 +1,18 @@
 import express from "express";
 import morgan from "morgan";
+/**JS 변수 Import */
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 const PORT = 4000;
 const app = express();
 const logger = morgan("dev");
-
-/**Middle ware */
-const routerLogger = (req, res, next) => {
-  console.log(`PATH ${req.path}`);
-  next();
-};
-const methodLogger = (req, res, next) => {
-  console.log(`METHOD ${req.method}`);
-  next();
-};
-/** application (get,request)설정 */
-const home = (req, res) => {
-  console.log("I will respond.");
-  return res.send("hello");
-};
-
-const login = (req, res) => {
-  return res.send("Login");
-};
 app.use(logger);
-app.get("/", home);
-app.get("/login", login);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
 /** 외부 접속 Listen*/
 const handleListening = () =>
   console.log(`Server listenting on port http://localhost:${PORT}👂`);
