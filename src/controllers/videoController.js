faa8b8b1d9be9cf937c0f891edbec0d613781441
ihanseul731 <61959771+YiHanSeul@users.noreply.github.com>
2022-01-11 11:29:38 +0900

    array

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,11 +1,36 @@
-export const trending = (req, res) => res.render("home");
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2minutes ago",
+    views: 89,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2minutes ago",
+    views: 89,
+    id: 1,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2minutes ago",
+    views: 89,
+    id: 1,
+  },
+];
+export const trending = (req, res) => {
+  return res.render("home", { pageTitle: "Home", videos });
+};
 export const see = (req, res) => res.render("watch");
 
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 
-export const deleteVideo = (req, res) => {
-  console.log(req.params);
-  return res.send("deleteVideo");
-};
+export const deleteVideo = (req, res) => res.send("deleteVideo");
