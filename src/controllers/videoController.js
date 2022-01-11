e493adc6a23199766ce57d0b80278b876ab6cc9b
ihanseul731 <61959771+YiHanSeul@users.noreply.github.com>
2022-01-11 13:32:54 +0900

    post,get edit controller

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,3 +1,5 @@
+import { is } from "express/lib/request";
+
 let videos = [
   {
     title: "First Video",
@@ -13,7 +15,7 @@ let videos = [
     comments: 2,
     createdAt: "2minutes ago",
     views: 89,
-    id: 1,
+    id: 2,
   },
   {
     title: "Third Video",
@@ -21,16 +23,26 @@ let videos = [
     comments: 2,
     createdAt: "2minutes ago",
     views: 89,
-    id: 1,
+    id: 3,
   },
 ];
 export const trending = (req, res) => {
   return res.render("home", { pageTitle: "Home", videos });
 };
-export const see = (req, res) => res.render("watch");
+export const watch = (req, res) => {
+  const { id } = req.params; // == const id=req.params.id;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching : ${video.title}`, video });
+};
 
-export const edit = (req, res) => res.render("edit");
-export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("Upload");
-
-export const deleteVideo = (req, res) => res.send("deleteVideo");
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("edit", { pageTitle: `Editing : ${video.title}`, video });
+};
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  videos[id - 1].title = title;
+  return res.redirect(`/videos/${id}`);
+};
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -7,7 +7,6 @@ const globalRouter = express.Router();
 globalRouter.get("/", trending);
 globalRouter.get("/join", join);
 globalRouter.get("/login", login);
-globalRouter.get("/search", search);
 
 /** globalRouter변수를 export해줌 */
 export default globalRouter;
