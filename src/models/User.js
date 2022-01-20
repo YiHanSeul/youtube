54af0a6d1763686962c66b19ae11c8ac0a7fb440
ihanseul731 <61959771+YiHanSeul@users.noreply.github.com>
2022-01-20 12:28:23 +0900

    password Hashing

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
@@ -6,6 +7,9 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
   name: { type: String, required: true },
   location: String,
+});
+userSchema.pre("save", async function () {
+  this.password = await bcrypt.hash(this.password, 5); //this.password는 user가 입력한 password임.
 });
 const User = mongoose.model("User", userSchema);
 export default User;
