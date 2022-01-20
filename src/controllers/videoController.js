f5f166f442a66184550e302f76eda68e08504ef7
ihanseul731 <61959771+YiHanSeul@users.noreply.github.com>
2022-01-20 16:13:31 +0900

    status code

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -31,7 +31,7 @@ export const postEdit = async (req, res) => {
   const video = await Video.exists({ _id: id });
   console.log(video);
   if (!video) {
-    return res.render("404", { pageTitle: "Video not found" });
+    return res.status(404).render("404", { pageTitle: "Video not found" });
   }
   await Video.findByIdAndUpdate(id, {
     title,
@@ -55,7 +55,7 @@ export const postUpload = async (req, res) => {
     });
     return res.redirect("/");
   } catch (error) {
-    return res.render("upload", {
+    return res.status(404).render("upload", {
       pageTitle: "Upload Video",
       errorMessage: error._message,
     });
